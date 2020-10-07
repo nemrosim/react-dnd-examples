3a349ad7e27a4399c575604a68ce6833003b2865
nemrosim <nemrosim1988@gmail.com>
2020-10-07 22:35:14 +0300

    create multiple items

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,27 @@ import './App.css';
 import { DndProvider, useDrag, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
-const MovableItem = ({setIsFirstColumn}) => {
+const MovableItem = ({name, setItems}) => {
+
+    const changeItemColumn = (currentItem, columnName) => {
+        setItems((prevState) => {
+            return prevState.map(e => {
+                return {
+                    ...e,
+                    column: e.name === currentItem.name ? columnName : e.column,
+                }
+            })
+        });
+    }
+
     const [{isDragging}, drag] = useDrag({
-        item: {name: 'Any custom name', type: 'Our first type'},
+        item: {name, type: 'Our first type'},
         end: (item, monitor) => {
             const dropResult = monitor.getDropResult();
-            if(dropResult && dropResult.name === 'Column 1'){
-                setIsFirstColumn(true)
+            if (dropResult && dropResult.name === 'Column 1') {
+                changeItemColumn(item, 'Column 1')
             } else {
-                setIsFirstColumn(false);
+                changeItemColumn(item, 'Column 2')
             }
         },
         collect: (monitor) => ({
@@ -23,7 +35,7 @@ const MovableItem = ({setIsFirstColumn}) => {
 
     return (
         <div ref={drag} className='movable-item' style={{opacity}}>
-            We will move this item
+            {name}
         </div>
     )
 }
@@ -43,19 +55,28 @@ const Column = ({children, className, title}) => {
 }
 
 export const App = () => {
-    const [isFirstColumn, setIsFirstColumn] = useState(true);
+    const [items, setItems] = useState([
+        {id: 1, name: 'Item 1', column: 'Column 1'},
+        {id: 2, name: 'Item 2', column: 'Column 1'},
+        {id: 3, name: 'Item 3', column: 'Column 1'},
+    ]);
 
-    const Item = <MovableItem setIsFirstColumn={setIsFirstColumn}/>;
+    const returnItemsForColumn = (columnName) => {
+        return items
+            .filter((item) => item.column === columnName)
+            .map((item) => (
+                <MovableItem key={item.id} name={item.name} setItems={setItems}/>
+            ))
+    }
 
     return (
         <div className="container">
-            {/* Wrap components that will be "draggable" and "droppable" */}
             <DndProvider backend={HTML5Backend}>
                 <Column title='Column 1' className='column first-column'>
-                    {isFirstColumn && Item}
+                    {returnItemsForColumn('Column 1')}
                 </Column>
                 <Column title='Column 2' className='column second-column'>
-                    {!isFirstColumn && Item}
+                    {returnItemsForColumn('Column 2')}
                 </Column>
             </DndProvider>
         </div>
