634a27eec0ae5b13a023ab7a51e2bf2135462406
nemrosim <nemrosim1988@gmail.com>
2020-10-07 21:10:12 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 
 const MovableItem = () => {
     const [{isDragging}, drag] = useDrag({
-        item: {name: 'Any custom name', type: 'MovableType'},
+        item: {name: 'Any custom name', type: 'Our first type'},
         collect: (monitor) => ({
             isDragging: monitor.isDragging(),
         }),
@@ -20,18 +20,9 @@ const MovableItem = () => {
     )
 }
 
-const FirstColumn = () => {
-    return (
-        <div className='column first-column'>
-            Column 1
-            <MovableItem/>
-        </div>
-    )
-}
-
-const SecondColumn = () => {
+const Column = ({children, className, title}) => {
     const [{canDrop, isOver}, drop] = useDrop({
-        accept: 'Not existing type',
+        accept: 'Our first type',
         drop: () => ({name: 'Some name'}),
         collect: (monitor) => ({
             isOver: monitor.isOver(),
@@ -42,8 +33,9 @@ const SecondColumn = () => {
     console.log('options', {canDrop, isOver});
 
     return (
-        <div ref={drop} className='column second-column'>
-            Column 2
+        <div ref={drop} className={className}>
+            {title}
+            {children}
         </div>
     )
 }
@@ -53,8 +45,12 @@ export const App = () => {
         <div className="container">
             {/* Wrap components that will be "draggable" and "droppable" */}
             <DndProvider backend={HTML5Backend}>
-                <FirstColumn/>
-                <SecondColumn/>
+                <Column title='Column 1' className='column first-column'>
+                    {<MovableItem/>}
+                </Column>
+                <Column title='Column 1' className='column second-column'>
+                    {null}
+                </Column>
             </DndProvider>
         </div>
     );
